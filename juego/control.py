169563a7a8a1dbc9fb36d8c5614ecef9cb1bfbe1
Ángel Roldán Rabanal <120009586+aroldanrabanal@users.noreply.github.com>
2024-10-23 08:25:00 +0200

    Update control.py

--- juego/control.py
+++ juego/control.py
@@ -55,31 +55,6 @@ def recompensa(personaje):
         print('Has encontrado un botín (150 almas)')
         personaje.oro += 150
 
-# def gachapon(tienda):
-#     g = randint(1, 4)
-#     if g == 1:
-#         print('Te ha tocado una espada de luz!!' + str(Personaje.ataque))
-#         Personaje.ataque += 4
-#     elif g == 2:
-#         print('Te ha tocado un arco luminoso!!' + 'Tu daño a aumentado a' + str(Personaje.ataque))
-#         Personaje.ataque += 3
-#     elif g == 3:
-#         print('Te ha tocado un tirachinas!!' + str(Personaje.ataque))
-#         Personaje.ataque += 0.5
-#     elif g == 4:
-#         print('Te ha tocado un moco, ¡TEN CUIDADO QUE ES PEGAJOSO!' + str(Personaje.ataque))
-#         Personaje.ataque -= 2
-
-
-
-# def tienda(j):
-#     respuesta = ''
-#     print('Ha aparecido la tienda de las sombras' + '¿Hay algo que te gustaria comprar?')
-#     respuesta = input('¿Que desas hacer?: g = gachapon(objeto aleatorio)(200 almas desoladas). / ')
-#     print('Tu oro' + str(Personaje.oro))
-
-#     if respuesta == g:
-#         gachapon()
         
         
 
